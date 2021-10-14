ed65510aa866a16b4df184aaf3afc430f44828c7
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-10-13 23:04:12 -0500

    rutas

---
+++ app/routes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+// Importar controladores
+const ProductController = require('../app/controllers/ProductController');
+
+// Products
+router.post('/', ProductController.create);
+router.get('/getByName/:name', ProductController.showByKeyWord);
+router.get('/', ProductController.sowAll);
+router.get('/:id', ProductController.showById);
+router.patch('/:id', ProductController.update);
+router.delete('/:id', ProductController.delete);
+
+//LOGIN
+
+
+
+
+
+
+
+
+
+
+//USUARIOS
+
+
+
+
+
+
+
+
+//VENTAS
+
+
+
+
+
+
+
+
+//CATEGORIAS
+
+module.exports = router;
+
+
--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var cors = require('cors')
-const { sequelize } = require('./models/index'); 
+//const { sequelize } = require('./models/index'); 
 
 // Setting
 const PORT = process.env.PORT || 5000;
@@ -12,14 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 // Rutas
-app.use(require('./routes'));
+//app.use(require('./routes'));
 
 
 // Arrancamos el servidor
 app.listen(PORT, function () {
     console.log(`La app ha arrancado en http://localhost:${PORT}`);
 
-    sequelize.sync({ force: false }).then(() => {
+  /*  sequelize.sync({ force: false }).then(() => {
         console.log("Se ha establecido la conexión");
-    })
+    })*/
 });
