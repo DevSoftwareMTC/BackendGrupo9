8c03e41a54b75f46f6ffcba3667455e113e7a884
nticorag <nticoragps4@gmail.com>
2021-10-15 00:12:32 -0500

    Conexion con front y agregar producto

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-/* const express = require('express');
+const express = require('express');
 const router = express.Router();
 
 // Importar controladores
@@ -45,24 +45,5 @@ router.delete('/:id', ProductController.delete);
 
 router.get('/getPrendas', ProductController.showAllPrendas);
 
-module.exports = router; */
-
-
-const express = require('express');
-const router = express.Router();
-
-// Importar controladores
-const ProductController = require('./controllers/ProductController');
-
-// Products
-router.post('/', ProductController.create);
-router.get('/getPrendas', ProductController.showAllPrendas);
-router.get('/getByName/:name', ProductController.showByKeyWord);
-router.get('/', ProductController.showAll);
-router.get('/:id', ProductController.showById);
-router.patch('/:id', ProductController.update);
-router.delete('/:id', ProductController.delete);
-
-
-
 module.exports = router;
+
