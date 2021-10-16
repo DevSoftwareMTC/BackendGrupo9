f58327ee8a1fd7f3be06d7edfbed5f4fb41d0ee9
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-10-16 18:04:26 -0500

    creacion del modulo categoria

--- app/models/Product.js
+++ app/models/Product.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define('Product', {
-    tipoPrenda: DataTypes.STRING(50),
+   // tipoPrenda: DataTypes.STRING(50),
     talla: DataTypes.STRING(3),
     nombreProducto: DataTypes.STRING,
     manufactura: DataTypes.STRING,
@@ -11,7 +11,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Product.associate = function(models) {
     // associations can be defined here
-    Product.belongsTo(models.tipoPrenda, { as: "tipo_Prenda", foreignKey: "prenda_id" });
+  //  Product.belongsTo(models.tipoPrenda, { as: "tipo_Prenda", foreignKey: "prenda_id" });
+     Product.belongsTo(models.Category, { as: "category", foreignKey: "category_id" });
   };
   return Product;
 };
