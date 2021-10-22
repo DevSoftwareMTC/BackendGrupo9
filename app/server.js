a1b080acd0aba2105e51fa923165066ef52967d6
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-10-22 18:54:54 -0500

    prueba

--- app/server.js
+++ app/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const { sequelize } = require('./models/index'); 
-
+app.use(cors());
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
